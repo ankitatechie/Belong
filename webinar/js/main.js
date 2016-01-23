1b40b5be5c9a871d80e09507f3b00533d9e16c99
ankitatechie <ankita.tech011@gmail.com>
2016-01-23 15:37:17 +0530

    smooth scrolling added

--- webinar/js/main.js
+++ webinar/js/main.js
@@ -21,10 +21,10 @@ $(document).ready(function() {
     	var width = $('.content').width();
     	$('.content').css('height',width+10);
 
-      	// Calculate width for profile pic hexagon
+      // Calculate width for profile pic hexagon
   		$('.profile .hexagon').css('width',width);
 
-      	// Calculate offset of li from body to keep the padding equal for the profile pic
+      // Calculate offset of li from body to keep the padding equal for the profile pic
       if($(window).width() > 480){
         var padding = $('.paddingLeft').offset().left;
         $('.biography .inner').css({'padding-left':padding, 'padding-right':padding});
@@ -38,6 +38,20 @@ $(document).ready(function() {
       }else{
         $('.navbar-default').css('opacity','1');
       }
-    }
+    };
+
+    // smooth scrolling
+    $('a[href^="#"]').on('click',function (e) {
+      e.preventDefault();
+
+      var target = this.hash;
+      var $target = $(target);
+
+      $('html, body').stop().animate({
+          'scrollTop': $target.offset().top
+      }, 900, 'swing', function () {
+          window.location.hash = target;
+      });
+    });
     
 });
